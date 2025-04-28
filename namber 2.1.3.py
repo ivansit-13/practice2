5e2241c47b59aa9965eeef75b743a4b1f01537d6
ivansit-13 <ivansitnicov18@gmail.com>
2025-04-28 19:21:04 +0700

    version4.1

--- namber 2.1.3.py
+++ namber 2.1.3.py
@@ -1,9 +1,16 @@
+def comparator(nums):
+    # а это тоже множество но с уникальными значениями)
+    newnums = set(nums)
+    # теперь сравниваем их длинну
+    if len(newnums) == len(nums):
+        return False
+    else:
+        return True
+
+
 # наше множество чисел в массиве
 nums = [1,2,3,4,5,1]
-# а это тоже множество но с уникальными значениями)
-newnums = set(nums)
-# теперь сравниваем их длинну
-if len(newnums) == len(nums):
-    print(False)
-else:
-    print(True)
+nums1 = [1,2,3,4,5,6]
+nums2 = [1,1,1,2,3,4,2]
+
+print(comparator(nums), comparator(nums1), comparator(nums2))
