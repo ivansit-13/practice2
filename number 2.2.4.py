9fffbc25d01419dcc9c405d4b2dc36c6daeb7cd9
ivansit-13 <ivansitnicov18@gmail.com>
2025-04-29 22:01:20 +0700

    version8.2

--- number 2.2.4.py
+++ number 2.2.4.py
@@ -8,9 +8,8 @@
 from random import randint
 
 class Counter:
-    def __init__(self,number,default):
+    def __init__(self,number):
         self.number = number
-        self.default = default
     def increase_number(self):
         self.number += 1
     def reduce_number(self):
@@ -19,8 +18,10 @@ class Counter:
         print(f"Ваше число на данный момент: {self.number}")
     def randomNumber(self):
         self.number = randint(1,100)
+    def default(self,number):
+        self.number = number
 x = int(input("Введите ваше число для счётчика "))
-number = Counter(x,x)
+number = Counter(x)
 exit_program = int(input("Если 1 - то вы хотите начать работу с счётчиком, иначе 0 - "))
 while exit_program == 1:
     print("Правила счётчика","\n",
@@ -28,7 +29,7 @@ while exit_program == 1:
           "2 - Счётчик-","\n",
           "3 - Счётчик информация на текущий момент","\n",
           "4 - Назначить рандомное значение", "\n",
-          "5 - Назначить число по умолчанию"
+          "5 - Назначить число по умолчанию", "\n",
           "0 - Выход")
     a = int(input())
     if a == 1:
@@ -41,6 +42,9 @@ while exit_program == 1:
         number.info()
     elif a == 4:
         number.randomNumber()
+        number.info()
+    elif a == 5:
+        number.default(x)
         number.info()
     elif a == 0:
         exit_program = int(input("Вы точно уверенны что хотите выйти? 1 - нет|| 0 - да "))
