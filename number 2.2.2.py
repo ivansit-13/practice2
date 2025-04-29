8d5cf66cdbdcd7430e30e697e6841630003d3594
ivansit-13 <ivansitnicov18@gmail.com>
2025-04-29 12:59:32 +0700

    version6.3

--- number 2.2.2.py
+++ number 2.2.2.py
@@ -8,6 +8,12 @@ class Train:
         self.name = name
         self.number = number
         self.time = time
+    def info_number(self):
+        return self.number
+    def changes_name(self, name):
+        self.name = name
+    def changes_time(self, time):
+        self.time = time
 
     def info(self):
         print(f"Названия пункта назначения: {self.name}")
@@ -25,8 +31,19 @@ Train2.info()
 print("\n","______________","\n")
 Train3.info()
 print("\n","______________","\n")
-exitprogram = int(input("Если 1 - то вы хотите редактировать, иначе 0 - "))
-while exitprogram != 0:
+exit_program = int(input("Если 1 - то вы хотите редактировать, иначе 0 - "))
+while exit_program != 0:
     a = int(input("Введите номер поезда "))
     for i in range(len(mas)):
-        if
+        if mas[i].info_number() == a:
+            print("Замена данных","\n")
+            newname = input("Названия пункта назначения: ")
+            mas[i].changes_name(newname)
+            newtime = input("Введите новое время поезда: ")
+            mas[i].changes_time(newtime)
+            print("\n", "______Новая_информация________", "\n")
+            mas[i].info()
+            print("\n", "______________________________", "\n")
+            continue
+
+    exit_program = int(input("Если 1 - то вы хотите продолжить редактировать, иначе 0 - "))
